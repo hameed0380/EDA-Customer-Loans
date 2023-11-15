2e8f9af59f065657b8d47cad0c7b6721c555a0f6
Hameed Roleola <hameed0380@gmail.com>
2023-11-15 17:56:41 +0000

    completed milestone 2 and added the relevant files. Also updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # EDA-Customer-Loans
+
+- ## Table of Contents
+- [EDA-Customer-Loans](#eda-customer-loans)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [File Structure](#file-structure)
+- [License](#license)
+
+
+## Description
 Exploratory Data Analysis - Customer Loans in Finance
+
+## Installation
+1. Clone the project repository from GitHub.
+
+## Usage
+Run the project using the instructions provided in the "Installation" section.
+
+## File Structure
+project_root/
+│
+├── main.py
+├── db_utils.py
+├── requirements.txt
+├── data.csv
+├── README.md
+└── config/
+    └── credentials.yaml
+
+## License
--- db_utils.py
+++ db_utils.py
@@ -7,21 +7,27 @@ import psycopg2
 class RDSDatabaseConnector:
 
     def __init__(self, credential) -> None:
-        self.loaded_creds = credential
+        self.loaded_creds = self.load_file(credential)
+        self.engine = self.initialise_engine()
 
     # def show_creds(self):
     #     print(self.loaded_creds)
 
-# template ideas
+    def load_file(self, credential):
+        '''Load credentials file to access the database
+        
+        Args:
+            credential: path of the credentials file to connect to the database
 
-# DATABASE_TYPE = 'postgresql'
-# DBAPI = 'psycopg2'
-# HOST = 'localhost'
-# USER = 'postgres'
-# PASSWORD = #'password'
-# DATABASE = 'pagila'
-# PORT = 5432
-# engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        Returns:
+            credential: returns a dict containing the credentials to
+            connect to the database.
+        '''
+        with open(credential, 'r') as file:
+            credential = yaml.safe_load(file)
+        print(credential)
+        return credential
+
 
     def initialise_engine(self):
         '''initialises a SQLAlchemy engine from the credentials provided from class'''
@@ -30,29 +36,47 @@ class RDSDatabaseConnector:
 
         DATABASE_TYPE = 'postgresql'
         HOST = creds["RDS_HOST"]
-        PASSWORD = ["RDS_PASSWORD"]
-        USER = ["RDS_USER"]
-        DATABASE = ["RDS_DATABASE"]
+        PASSWORD = creds["RDS_PASSWORD"]
+        USER = creds["RDS_USER"]
+        DATABASE = creds["RDS_DATABASE"]
         PORT = 5432
 
         engine = create_engine(f"{DATABASE_TYPE}+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
         return engine
     
-    def extract_dataframe(self):
-        pass
+    def extract_data(self, table_name):
+        '''Extracts the data from RDS database and returns it as a DataFrame.
+        
+        Args:
+            table_name: name of the table we want to extract from
+        Returns:
+            data: returns the data we queried
+        '''
+        data = pd.read_sql_query(f"SELECT * FROM {table_name}", self.engine)
+        return data
+    
+    def save_to_csv(self, data, credential):
+        '''Save the data to a csv file'''
+        data.to_csv(credential, index=False)
+
+    def load_csv(self, data_csv):
+        '''Load the data csv as a pandas dataframe'''
+        df = pd.read_csv(data_csv)
+        return df
 
 
 
-def load_file():
-    '''Load  credentials file to access the database'''
-    with open('credentials.yaml', 'r') as file:
-        credential = yaml.safe_load(file)
-    print(credential)
-    return credential
+
+
 
 
 if __name__ == '__main__':
-    # Load credentials from your YAML file
-    credentionals_passed = load_file()
-    RDSDatabaseConnector(credentionals_passed).initialise_engine()
+    RDS_CONNECTOR = RDSDatabaseConnector('credentials.yaml')
+    RDS_CONNECTOR.initialise_engine()
+    data = RDS_CONNECTOR.extract_data('loan_payments')
+    print(data.head())
+    RDS_CONNECTOR.save_to_csv(data, 'data.csv')
+    dataframe = RDS_CONNECTOR.load_csv('data.csv')
+    print(dataframe)
+    print(dataframe.shape)
 
