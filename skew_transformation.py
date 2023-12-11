f6bd0926aa129712f9f0d1e620247196aace84b3
Hameed Roleola <hameed0380@gmail.com>
2023-12-11 00:54:21 +0000

    changed the skew method to log and added identifier

--- skew_transformation.py
+++ skew_transformation.py
@@ -1,25 +1,30 @@
-import pandas as pd
 import numpy as np
-from scipy import stats
+import scipy.stats as stats
 
 class SkewTransform:
+    # TODO add Yeo Johnson transform
+
     def __init__(self, df):
         self.df = df
+    
+    def identify_skewed_columns(self, threshold=2):
+        '''
+        Function applies the .skew() method to each column the dataframe
+        and returns a list of columns with a skew above a given threshold. 
 
-    def boxcox_transform(self, column):
+        Threshold: The threshold value to filter the returned column list. 
+        '''
+        numerical_columns = self.df.select_dtypes(include=[np.number]).columns
+        skewness = self.df[numerical_columns].apply(lambda n: n.skew())
+        skewed_columns = skewness[abs(skewness) > threshold].index.tolist()
+        return skewed_columns
+    
+    def transform_log(self, threshold=2):
+        '''
+        Function applies log transformation to list of skewed columns. Occurs inplace.
 
-        skew = stats.skew(self.df[column])
-        if skew > 1:
-            boxcox_sample = self.df[column]
-            
-            boxcox_transform = stats.boxcox(boxcox_sample)
-            boxcox_transform = pd.Series(boxcox_transform[0])
-            self.df[column] = boxcox_transform
-
-        
-    def yeojohnson_transform(self, column):
-
-        yeojohnson_transform = self.df[column]
-        yeojohnson_transform = stats.yeojohnson(yeojohnson_transform)
-        yeojohnson_transform = pd.Series(yeojohnson_transform[0])
-        self.df[column] = yeojohnson_transform
+        Threshold: The threshold passed to the identify_skewed_columns() function.
+        '''
+        skewed_columns = self.identify_skewed_columns(threshold)
+        for column in skewed_columns: 
+            self.df[column] = self.df[column].map(lambda i: np.log1p(i) if i > 0 else 0)
